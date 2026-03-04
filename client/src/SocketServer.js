f3df9ad4752cb4a0aab8f1337f1d78457ab822af
Abel Addis <abeladdis3@gmail.com>
2026-03-03 19:50:13 -0500

    removed multiplayer

--- client/src/SocketServer.js
+++ client/src/SocketServer.js
@@ -1,5 +1,3 @@
-import * as Colyseus from "colyseus.js";
-
 /*================================================
 | Array with current online players
 */
@@ -8,13 +6,6 @@ let onlinePlayers = {};
 /*================================================
 | Colyseus connection with server
 */
-var client = new Colyseus.Client('ws://localhost:3000');
-let room = client.joinOrCreate("poke_world").then(room => {
-    console.log(room.sessionId, "joined", room.name);
-    return room
-}).catch(e => {
-    console.log("JOIN ERROR", e);
-    throw e;
-});
+let room = Promise.resolve(null);
 
 export {onlinePlayers, room};
